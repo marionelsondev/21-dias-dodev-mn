05805c502dd4c44738bee59afd56900c395dace5
Mário Nelson <marionelson616@gmail.com>
2024-03-29 17:59:49 -0400

    Atualizando o projeto 2 'Concessionária' foi adicionado o algoritmo de ordenação(bubble sort) para ordenar os vetores dos valores em ordem crescente.

--- dia-11/Concessionaria/script.js
+++ dia-11/Concessionaria/script.js
@@ -1,8 +1,12 @@
+// DECLARANDO VARIÁVEIS
+
 let modelos = [];
 let anos = [];
 let valores = [];
 let continuar = true
 let elemento = 0;
+
+// CADASTRANDO OS CARROS -> MODELO, ANO, VALOR
 
 while (continuar) {
     let modelo = prompt(`Qual o modelo do ${elemento + 1}º carro?`);
@@ -22,6 +26,37 @@ while (continuar) {
     }
 }
 
+// EXIBINDO OS CARROS CADASTRADOS
+
+console.log("Carros cadastrados:")
 for (i = 0; i < modelos.length; i++) {
+    console.log(`${modelos[i]} - ${anos[i]}\nValor: ${valores[i]}`);
+}
+
+// ALGORITMO DE ORDENAÇÃO(BUBBLE SORT) PARA ORDENAR OS VALORES DOS CARROS EM ORDEM CRESCENTE
+
+for (let contadorEscopoGlobal = 0; contadorEscopoGlobal < valores.length - 1; contadorEscopoGlobal++) {
+    for (let contadorProcesso = 0; contadorProcesso < valores.length - contadorEscopoGlobal - 1; contadorProcesso++) {
+        if (valores[contadorProcesso] > valores[contadorProcesso + 1]) {
+
+            let maiorValorModelo = modelos[contadorProcesso]
+            modelos[contadorProcesso] = modelos[contadorProcesso + 1]
+            modelos[contadorProcesso + 1] = maiorValorModelo
+
+            let maiorValorAno = anos[contadorProcesso]
+            anos[contadorProcesso] = anos[contadorProcesso + 1]
+            anos[contadorProcesso + 1] = maiorValorAno
+
+            let maiorValor = valores[contadorProcesso]
+            valores[contadorProcesso] = valores[contadorProcesso + 1]
+            valores[contadorProcesso + 1] = maiorValor
+        }
+    }
+}
+
+// EXIBINDO OS CARROS CADASTRADOS ORDENADOS POR PREÇO EM ORDEM CRESCENTE
+
+console.log("Carros ordenados por preço:")
+for (i = 0; i < valores.length; i++) {
     console.log(`${modelos[i]} - ${anos[i]}\nValor: ${valores[i]}`);
 }
